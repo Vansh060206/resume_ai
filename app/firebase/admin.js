0e36e0ec4687d4db0ee6628dc822583df7694581
Mankani vansh <mankanivansh273@gmail.com>
2026-01-27 23:25:06 +0530

    Fix firebase admin env var names

--- app/firebase/admin.js
+++ app/firebase/admin.js
@@ -3,10 +3,10 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.PROJECTID,
-      clientEmail: process.env.CLIENTEMAIL,
-      privateKey: process.env.PRIVATEKEY
-        ? process.env.PRIVATEKEY.replace(/\\n/g, "\n")
+      projectId: process.env.NEXT_PUBLIC_PROJECTID,
+      clientEmail: process.env.NEXT_PUBLIC_clientEmail,
+      privateKey: process.env.NEXT_PUBLIC_privateKey
+        ? process.env.NEXT_PUBLIC_privateKey.replace(/\\n/g, "\n")
         : undefined,
     }),
     databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
